c8fd388dce70d4a333f5678c9cbc6fd5f40fb36f
louis-driver <90572825+louis-driver@users.noreply.github.com>
2023-02-24 16:29:55 -0600

    Contains html and script

--- chapter08/project2/js/ch08-proj02.js
+++ chapter08/project2/js/ch08-proj02.js
@@ -1,1 +1,44 @@
 /* add your code here */
+//Parse content from photo-data.js
+const photos = JSON.parse(content);
+
+for (let p of photos)
+{
+    console.log(outputCard(p));
+    document.write(outputCard(p));
+}
+
+function outputCard(photo)
+{
+    //writes the entire html for that given card
+    let card = '<article><img src=images/' + photo.filename + ' alt="'
+    + photo.title + '"><div class="caption"><h2>' + photo.title + '</h2><p>'
+    + photo.location.city + ', ' + photo.location.country + '</p><h3>Colors</h3>'
+    + outputColors(photo.colors) + '</div></article>';
+    return card;
+}
+
+function outputColors(colors)
+{
+    let result = "";
+    for (let c of colors)
+    {
+        result += constructColor(c) + '\n';
+    }
+    return result;
+}
+
+function constructColor(color)
+{
+    let result = '<span style="' + constructStyle(color) + '">'
+    + color.name + '</span>';
+    return result;
+}
+
+function constructStyle(color)
+{
+    let result = "background-color:" + color.hex + ";";
+    if (color.luminance < 70)
+        result += "color:white";
+    return result;
+}
