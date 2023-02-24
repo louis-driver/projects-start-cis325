19fc6d4cdb444f572606bff67b371d4257b8b14e
louis-driver <90572825+louis-driver@users.noreply.github.com>
2023-02-24 16:35:01 -0600

    Formatting

--- chapter08/project2/js/ch08-proj02.js
+++ chapter08/project2/js/ch08-proj02.js
@@ -1,16 +1,16 @@
-/* add your code here */
 //Parse content from photo-data.js
 const photos = JSON.parse(content);
 
+//Write all cards to the html document
 for (let p of photos)
 {
     console.log(outputCard(p));
     document.write(outputCard(p));
 }
 
+//Writes the entire html for a given card
 function outputCard(photo)
 {
-    //writes the entire html for that given card
     let card = '<article><img src=images/' + photo.filename + ' alt="'
     + photo.title + '"><div class="caption"><h2>' + photo.title + '</h2><p>'
     + photo.location.city + ', ' + photo.location.country + '</p><h3>Colors</h3>'
@@ -18,16 +18,18 @@ function outputCard(photo)
     return card;
 }
 
+//Creates html for all colors in a card
 function outputColors(colors)
 {
     let result = "";
     for (let c of colors)
     {
-        result += constructColor(c) + '\n';
+        result += constructColor(c);
     }
     return result;
 }
 
+//Creates the html for a color
 function constructColor(color)
 {
     let result = '<span style="' + constructStyle(color) + '">'
@@ -35,6 +37,7 @@ function constructColor(color)
     return result;
 }
 
+//Defines the CSS style for a color
 function constructStyle(color)
 {
     let result = "background-color:" + color.hex + ";";
