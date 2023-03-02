8db7fa9bd6a6cdcf46717919e9de1d770f4460f1
louis-driver <90572825+louis-driver@users.noreply.github.com>
2023-03-01 20:46:39 -0600

    Draws rectangles for each feature of a painting

--- chapter09/project2/ch09-proj2.js
+++ chapter09/project2/ch09-proj2.js
@@ -1,16 +1,19 @@
 /* add your code here */
 document.addEventListener("DOMContentLoaded", function()
 {
+    function f(selector){
+        return document.querySelector(selector);
+    }
     const paintings = JSON.parse(content);
     console.log(paintings);
 
     const parent = document.querySelector("#paintings ul");
+    //Adds all images in paintings[] to the sidebar list
     for (let i=0; i<paintings.length; i++)
     {
         let child = document.createElement("li");
         let thumbnail = document.createElement("img");
         thumbnail.dataset.key = paintings[i].id;
-        console.log(thumbnail.dataset.key);
         thumbnail.src = "images/small/" + paintings[i].id + ".jpg"
         child.appendChild(thumbnail);
         parent.appendChild(child);
@@ -20,17 +23,35 @@ document.addEventListener("DOMContentLoaded", function()
     parent.addEventListener("click", function(e){
         if (e.target && e.target.nodeName == "IMG")
         {
-            console.log(document.querySelector("#details").innerHTML);
             //update the details section with e's info
-            let key = e.target.dataset.key;
             let painting;
             for (let i=0; i<paintings.length; i++)
             {
-                if (paintings[i].id == key)
+                if (paintings[i].id == e.target.dataset.key)
                     painting = paintings[i];
             }
-            document.querySelector("#title").textContent = painting.title;
-            document.querySelector("#artist").textContent = painting.artist;
+            f("#title").textContent = painting.title;
+            f("#artist").textContent = painting.artist;
+            //Add large image to the html
+            let figure = f("#details figure");
+            figure.innerHTML = "";
+            let image = document.createElement("img");
+            image.src = "images/large/" + painting.id + ".jpg"
+            figure.appendChild(image);
+
+            //Draw a rectangle for each feture in the painting
+            for (let i=0; i<painting.features.length; ++i)
+            {
+                let feature = painting.features[i]
+                let box = document.createElement("div");
+                box.className = "box";
+                box.style = "position:absolute;left:"+feature.upperLeft[0]+"px;top:"
+                    +feature.upperLeft[1]+"px;width:"+(feature.lowerRight[0]-feature.upperLeft[0])
+                    +"px;height:"+(feature.lowerRight[1]-feature.upperLeft[1])+"px;";
+                console.log(box.style);
+                figure.appendChild(box);
+            }
+
         }
     })
 
