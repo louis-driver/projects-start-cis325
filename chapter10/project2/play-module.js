333b3daf2fb32cd02e4e873231a45ced54ec654c
louis-driver <90572825+louis-driver@users.noreply.github.com>
2023-03-10 09:53:13 -0600

    Classes assign values from play data

--- chapter10/project2/play-module.js
+++ chapter10/project2/play-module.js
@@ -1,2 +1,59 @@
 /* In this module, create three classes: Play, Act, and Scene. */
 
+export class Play
+{
+    constructor(content)
+    {
+        //Assign values
+        this.title = content.title;
+        this.titleShort = content.short;
+        this.persona = content.persona;
+        this.acts = new Array();
+        for (let act of content.acts) {
+            console.log(act);
+            this.acts.push(new Act(act));
+        }   
+    }
+
+    print()
+    {
+        console.log(this.title);
+        console.log(this.titleShort);
+        console.log(this.acts)
+    }
+    //Some function
+    //functionName() { do something / return something}
+}
+
+class Act
+{
+    constructor(act)
+    {
+        //Assign values
+        this.name = act.name;
+        this.scenes = new Array();
+        for (let scene of act.scenes) {
+            this.scenes.push(scene);
+        }
+
+    }
+    //Some function
+    //functionName() { do something / return something}
+}
+
+class Scene
+{
+    constructor(scene)
+    {
+        //Assign values
+        this.name = scene.name;
+        this.title = scene.title;
+        this.stageDirection = scene.stageDirection;
+        this.speeches = new Array();
+        for (let speech of scene.speeches) {
+            this.speeches.push(speech);
+        }
+    }
+    //Some function
+    //functionName() { do something / return something}
+}
