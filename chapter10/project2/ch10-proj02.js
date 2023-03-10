8c55dce5bcff5462351fd27063db29003477dcc0
louis-driver <90572825+louis-driver@users.noreply.github.com>
2023-03-10 11:21:15 -0600

    Working with select lists

--- chapter10/project2/ch10-proj02.js
+++ chapter10/project2/ch10-proj02.js
@@ -4,12 +4,8 @@ async function getPlay(url)
 {
    try {
       let response = await fetch(url);
-      console.log(response);
       let data = await response.json();
-      console.log(typeof data);
-      console.log(data);
       let play = new plays.Play(data);
-      play.print();
       return play;
    }
    catch (err) {
@@ -18,9 +14,67 @@ async function getPlay(url)
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-
+   function qs(selector) {
+      return document.querySelector(selector);
+   }
+   function qsa(selector) {
+      return document.querySelectorAll(selector);
+   }
+   function ce(element) {
+      return document.createElement(element);
+   }
 	
 	const url = 'https://www.randyconnolly.com//funwebdev/3rd/api/shakespeare/play.php';
+
+   const playList = qs('#playList');
+   let actList = qs('#actList');
+   //TODO maybe separate each selectList into their own event listener
+   playList.addEventListener('change', async function(e)
+   {
+      //get the play
+      let playUrl = url + '?name=' + e.target.value;
+      let play = await getPlay(playUrl);
+      console.log(play);
+
+      //populate the title
+      qs('#playHere h2').textContent = play.title;
+
+      let actList = qs('#actList');
+      actList.innerHTML = '';
+      //populate the select list for the acts
+      let option = ce('option');
+      option.textContent = 'ACTS';
+      actList.appendChild(option);
+      for (let act of play.acts)
+      {
+         option = ce('option');
+         option.textContent = act.name;
+         actList.appendChild(option);
+      }
+
+      //find scenes for current act
+      let currAct = qs('#actList').selected;
+      //console.log(currAct);
+      //populate select list for scene
+      //for (let scene of )
+      //populate select list for characters
+      //update display of current scene
+   });
+
+   
+   actList.addEventListener('change', function (e)
+   {
+      //TODO get the current act from select list
+      console.log(e.target.value);
+      //populate scenes based on current act
+   });
+
+   let sceneList = qs('#sceneList');
+   sceneList.addEventListener('change', function (e)
+   {
+      //get the current scene
+      //populate the html based on the scene data
+   });
 
    /*
      To get a specific play, add play name via query string, 
