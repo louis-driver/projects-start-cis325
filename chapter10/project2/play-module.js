1e54f6c41fc428f8e875f4007941e41273b82974
louis-driver <90572825+louis-driver@users.noreply.github.com>
2023-03-11 17:02:28 -0600

    Referenced code from profesor

--- chapter10/project2/play-module.js
+++ chapter10/project2/play-module.js
@@ -1,17 +1,26 @@
 /* In this module, create three classes: Play, Act, and Scene. */
 
+function qs(selector) {
+    return document.querySelector(selector);
+ }
+ function qsa(selector) {
+    return document.querySelectorAll(selector);
+ }
+ function ce(element) {
+    return document.createElement(element);
+}
+
 export class Play
 {
-    constructor(content, document)
+    constructor(content)
     {
         //Assign values
-        this.document = document;
         this.title = content.title;
         this.titleShort = content.short;
         this.persona = content.persona;
         this.acts = new Array();
         for (let act of content.acts) {
-            this.acts.push(new Act(act, document));
+            this.acts.push(new Act(act));
         }   
     }
 
@@ -23,14 +32,25 @@ export class Play
     }
     //Some function
     //functionName() { do something / return something}
+    makeMarkup(playContainer, actContainer, sceneContainer)
+    {
+        console.log(playContainer);
+        console.log(actContainer);
+        console.log(sceneContainer);
+        playContainer.innerHTML = '';
+        let playTitle = ce('h2');
+        playTitle.textContent = this.title;
+        playContainer.appendChild(playTitle);
+        this.acts[0].makeMarkup(playContainer, actContainer, sceneContainer);
+        console.log(playContainer);
+    }
 }
 
-class Act
+export class Act
 {
-    constructor(act, document)
+    constructor(act)
     {
         //Assign values
-        //this.document = document;
         this.name = act.name;
         this.scenes = new Array();
         for (let scene of act.scenes) {
@@ -39,13 +59,18 @@ class Act
     }
     //Some function
     //functionName() { do something / return something}
-    makeMarkup()
+    makeMarkup(playContainer, actContainer, sceneContainer)
     {
-        return 0;
+        console.log(actContainer);
+        actContainer.innerHTML = '';
+        let actName = ce('h3')
+        actName.textContent = this.name;
+        this.scenes[0].makeMarkup(actContainer, sceneContainer, 1, 1);
+        playContainer.appendChild(actContainer);
     }
 }
 
-class Scene
+export class Scene
 {
     constructor(scene)
     {
@@ -59,16 +84,56 @@ class Scene
             this.speeches.push(speech);
         }
     }
-    //Some function
-    //functionName() { do something / return something}
-    makeMarkup()
-    {
-        let div = document.createElement('div');
-        div.id = 'sceneHere';
-        let sceneName = document.createElement('h4');
-        sceneName.textContent = this.name;
 
-        div.appendChild(sceneName);
-        document.querySelector('#actHere').appendChild(div);
+    makeMarkup(actContainer, container, search, player)
+    {
+        container.innerHTML = '';
+        let sceneName = ce('h4');
+        sceneName.textContent = this.name;
+        let sceneTitle = ce('p');
+        sceneTitle.classList.add('title');
+        sceneTitle.textContent = this.title;
+        let direction = ce('p');
+        direction.classList.add('direction');
+        direction.textContent = this.stageDirection;
+
+        container.appendChild(sceneName);
+        container.appendChild(sceneTitle);
+        container.appendChild(direction);
+
+        this.outputSpeeches(container, search, player);
+        actContainer.appendChild(container);
+    }
+
+    outputSpeeches(container, search, player)
+    {
+        const divs = this.speeches.map( sp => {
+            
+            let div = ce('div');
+            div.classList.add('speech');
+            //if (player && sp.speaker != player && player != 0)
+                //return div;
+            
+            let speaker = ce('span');
+            speaker.textContent = sp.speaker;
+            div.appendChild(speaker);
+
+            let lines = sp.lines.map( ln => {
+                let line = ce('p');
+                line.textContent = ln;
+                return line;
+            })
+
+            lines.forEach(ln => div.appendChild(ln));
+
+            if (sp.stagedir){
+                let em = ce('em');
+                em.textContent = sp.stagedir;
+                div.appendChild(em);
+            }
+
+            return div;
+        })
+        divs.forEach( div => container.appendChild(div));
     }
 }
