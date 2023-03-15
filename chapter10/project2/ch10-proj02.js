e0677754793263398c8e779fdd270a348ee370dc
louis-driver <90572825+louis-driver@users.noreply.github.com>
2023-03-15 10:22:37 -0500

    Cleaned up code

--- chapter10/project2/ch10-proj02.js
+++ chapter10/project2/ch10-proj02.js
@@ -49,6 +49,7 @@ document.addEventListener("DOMContentLoaded", function() {
    let currPlayer = '';
    playList.addEventListener('change', async function(e)
    {
+      currPlayer = '';
       //Only update if a play is selected
       if (e.target.value != 0) {
       //get the play
@@ -56,9 +57,7 @@ document.addEventListener("DOMContentLoaded", function() {
       play = await getPlay(playUrl);
 
       play.makeMarkup(playContainer, actContainer, sceneContainer, currPlayer);
-      //console.log(document);
-   
-      let option;
+
       //populate the select list for the acts
       populateNameList(actList, play.acts);
    
@@ -66,6 +65,7 @@ document.addEventListener("DOMContentLoaded", function() {
       populateNameList(sceneList, play.acts[0].scenes);
    
       //populate select list for characters
+      let option;
       playerList.innerHTML = '<option value=0>All Players</option>';
       for (let persona of play.persona)
       {
@@ -89,117 +89,26 @@ document.addEventListener("DOMContentLoaded", function() {
    //update display of current scene
    sceneList.addEventListener('change', function (e)
    {
-      let currAct = play.acts[actList.selectedIndex];
       //get the current scene
-      let currScene = currAct.scenes[e.target.selectedIndex];
+      let currScene = play.acts[actList.selectedIndex].scenes[e.target.selectedIndex];
       //populate the html based on the scene data
-      //Calls the Play.makeMarkup()???
-      //currScene.makeMarkup(playContainer, actContainer, sceneContainer);
       let search;
       currScene.makeMarkup(actContainer, sceneContainer, search, currPlayer);
-      //console.log(document);
    });
 
    playerList.addEventListener('change', function (e)
    {
+      //get selected character
       if (e.target.selectedIndex != 0)
          currPlayer = play.persona[e.target.selectedIndex-1].player;
       else
          currPlayer = '';
       
+      //Update scene to reflect only that character's lines
       let currScene = play.acts[actList.selectedIndex].scenes[sceneList.selectedIndex];
       let search = '';
       currScene.makeMarkup(actContainer, sceneContainer, search, currPlayer);
    });
-
-   /*
-   playList.addEventListener('change', async function(e)
-   {
-      actList.innerHTML = '';
-      sceneList.innerHTML = '';
-      playerList.innerHTML = '<option value=0>All Players</option>';
-      if (e.target.value != 0) {
-      //get the play
-      let playUrl = url + '?name=' + e.target.value;
-      play = await getPlay(playUrl);
-
-      //populate initial play information
-      play.makeMarkup(qs('#playHere'), qs('#actHere'), qs('#sceneHere'));
-      //updateScene(play, play.acts[0], play.acts[0].scenes[0]);
-
-      let option;
-      //populate the select list for the acts
-      for (let act of play.acts)
-      {
-         option = ce('option');
-         option.textContent = act.name;
-         actList.appendChild(option);
-      }
-
-      //Set initial scene to the first scene of the first act
-      for (let scene of play.acts[0].scenes)
-      {
-         option = ce('option');
-         option.textContent = scene.name;
-         sceneList.appendChild(option);
-      }
-
-      //find scenes if act changes
-      actList.addEventListener('change', function (e)
-      {
-         //gets the current act from select list
-         let currAct = play.acts.find(act => act.name === e.target.value);
-         //updateScene(play, currAct, currAct.scenes[0]);
-         play.makeMarkup(qs('#playHere'), qs('#actHere'), qs('#sceneHere'));
-
-         //populate scenes based on current act
-         sceneList.innerHTML = '';
-         let option;
-         for (let scene of currAct.scenes)
-         {
-            option = ce('option');
-            option.textContent = scene.name;
-            sceneList.appendChild(option);
-         }
-
-         //update display of current scene
-         sceneList.addEventListener('change', function (e)
-         {
-            //get the current scene
-            let currScene = currAct.scenes.find(scene => scene.name === e.target.value);
-            //populate the html based on the scene data
-            //updateScene(play, currAct, currScene);
-            console.log(currScene);
-            play.makeMarkup(qs('#playHere'), qs('#actHere'), qs('#sceneHere'));
-         });
-      });
-
-      //update display of current scene when act hasn't changed
-      sceneList.addEventListener('change', function (e)
-      {
-         //get the current scene
-         let currScene = play.acts[0].scenes.find(scene => scene.name === e.target.value);
-         //populate the html based on the scene data
-         //updateScene(play, play.acts[0], currScene);
-         play.makeMarkup(qs('#playHere'), qs('#actHere'), qs('#sceneHere'));
-      });
-
-      //populate select list for characters
-      for (let persona of play.persona)
-      {
-         option = ce('option');
-         option.textContent = persona.player;
-         playerList.appendChild(option);
-      }
-
-      //Find selected player and their scenes
-      playerList.addEventListener('change', function (e)
-      {
-         let currPlayer = play.persona.find(persona => persona.player === e.target.value);
-      });
-
-   } 
-   });  */
 
    /*
      To get a specific play, add play name via query string, 
@@ -216,12 +125,4 @@ document.addEventListener("DOMContentLoaded", function() {
        Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
        use built-in Live Preview.
     */
-
-   //Add event listeners to update the DOM
-   //const play = new plays.Play(jcaesar);
-   //console.log(play.title);
-
-   let currUrl = url + '?name=jcaesar';
-   const jcaesar = getPlay(currUrl); //getPlay isn't fullfilled when called, so jcaesar can't be used yet
-   // go to page 521
 });
