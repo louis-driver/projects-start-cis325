54820a8525595e7a27128f3b890d0017308e7d60
louis-driver <90572825+louis-driver@users.noreply.github.com>
2023-04-16 16:32:17 -0500

    included all data bc idk what the adoption means

--- chapter14/project2/models/Adoption.js
+++ chapter14/project2/models/Adoption.js
@@ -2,14 +2,37 @@ const mongoose = require ('mongoose');
 //Define a schema that maps to the structure of the data in MongoDB
 const adoptionSchema = new mongoose.Schema({
     id: Number,
-    date: Date,
-    quantity: Number,
-    books: {
+    gui: String,
+    contacts: {
+        order: Number,
+        gender: String,
+        firstname: String,
+        lastname: String
+    },
+    university: {
         id: Number,
-        isbn10: String,
-        isbn13: Number,
-        title: String,
-        Category: String
+        name: String,
+        address: String,
+        city: String,
+        state: String,
+        website: String,
+        zip: String,
+        longitude: Number,
+        latitude: Number,
+        classification: String
+
+    },
+    adoptions: {
+        id: Number,
+        date: Date,
+        quantity: Number,
+        books: {
+            id: Number,
+            isbn10: String,
+            isbn13: Number,
+            title: String,
+            Category: String
+        }
     }
 });
 //Create model using the schema that maps to adoptions collection in database
