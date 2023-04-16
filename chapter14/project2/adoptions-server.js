2c53d14bcb755962dbc366fe90db368567563b33
louis-driver <90572825+louis-driver@users.noreply.github.com>
2023-04-16 14:05:08 -0500

    Created connection with MongoDB

---
+++ chapter14/project2/adoptions-server.js
@@ -0,0 +1,12 @@
+
+require('dotenv').config();
+console.log(process.env.MONGO_URL);
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('connected to mongo');
+});
